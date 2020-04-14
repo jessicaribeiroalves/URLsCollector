6b67371238fb3403ceb87dec9785996dd51b072e
Jessica Ribeiro Alves <jessralves@gmail.com>
2020-04-13 21:52:05 -0300

    update README and remove comments from code

--- __tests__/getLinks.test.js
+++ __tests__/getLinks.test.js
@@ -9,17 +9,14 @@ jest.mock('../src/httpRequest', () => ({
 
 describe('getLinks', () => {
   it('should call getHtml(), extractAllLinks() and return a links array', async () => {
-    // setup
     const html = 'html text';
     const url = 'https://www.google.com';
     const links = ['url1', 'url2', 'url3'];
     getHtml.mockReturnValue(Promise.resolve(html));
     extractAllLinks.mockReturnValue(Promise.resolve(links))
 
-    // execution
     const results = await getLinks(url);
 
-    // assertions
     expect(getHtml).toHaveBeenCalledTimes(1);
     expect(getHtml).toHaveBeenCalledWith(url);
     expect(extractAllLinks).toHaveBeenCalledTimes(1);
